cc9e968da52aac6b40d0e40bc35586939374828a
shengyun-zhou <GGGZ-1101-28@live.cn>
2021-11-12 10:29:42 +0800

    Exclude redundant static libs from FreeBSD sysroot

--- prebuilt-freebsd/prepare_sysroot.sh
+++ prebuilt-freebsd/prepare_sysroot.sh
@@ -8,11 +8,11 @@ check_build_for_targets 'freebsd' || exit 0
 cd "$PRE_PWD"
 
 for arch in arm64 i386 amd64; do
-    mkdir .freebsd-sysroot && cd .freebsd-sysroot
+    mkdir -p .freebsd-sysroot && cd .freebsd-sysroot
     curl -L "http://mirrors.ustc.edu.cn/freebsd/releases/$arch/${FREEBSD_VERSION}-RELEASE/base.txz" -o sysroot.tar.xz
     tar_extractor.py sysroot.tar.xz
     tar cvzf ../freebsd-sysroot-${FREEBSD_VERSION}_$arch.tar.gz ./lib ./usr/include ./usr/lib \
         --exclude=./usr/include/c++* --exclude=./usr/include/gcc --exclude=./usr/lib/clang --exclude=./usr/lib/libcompiler_rt* \
-        --exclude=./usr/lib/libc++* --exclude=./usr/lib/libgcc*.a
+        --exclude=./usr/lib/libc++* --exclude=./usr/lib/libgcc*.a --exclude=./usr/lib/*_p.a
     cd .. && rm -rf .freebsd-sysroot
 done
