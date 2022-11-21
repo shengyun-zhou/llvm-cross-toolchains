34872ce1d9a4dd6362cf4a923b7f81874f68afa7
Shengyun Zhou <GGGZ-1101-28@live.cn>
2022-11-21 11:28:12 +0800

    Refactor the Windows docker image to use llvm-mingw

--- docker/ct-clang-mingw-wrapper.sh
+++ docker/ct-clang-mingw-wrapper.sh
@@ -5,15 +5,13 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 BASENAME="$(basename "$0")"
 TARGET="${BASENAME%-*}"
 EXE="${BASENAME##*-}"
-GCC_VERSION=11.2.0
 
-CLANG_ARGS=(-Qunused-arguments -target $TARGET --sysroot "$DIR/../$TARGET/sysroot/mingw" 
-    -L "$DIR/../lib/gcc/$TARGET/$GCC_VERSION" -L "$DIR/../$TARGET/sysroot/lib" -static-libgcc)
+CLANG_ARGS=(-Qunused-arguments -target $TARGET --sysroot "$DIR/../$TARGET" -rtlib=compiler-rt -unwindlib=libunwind -stdlib=libc++ -fuse-ld=ld)
 
 if [[ $EXE == "clang++" ]]; then
-    EXE=clang++
+    EXE=/usr/bin/clang++
 else
-    EXE=clang
+    EXE=/usr/bin/clang
 fi
 
 $EXE "${CLANG_ARGS[@]}" "$@" -Wno-unused-but-set-variable
