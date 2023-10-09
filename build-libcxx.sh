865bf8375de29f9bb1d0cb1bf82a1f04b86adc51
Shengyun Zhou <GGGZ-1101-28@live.cn>
2023-10-09 14:08:13 +0800

    Build and use shared libc++ by default

--- build-libcxx.sh
+++ build-libcxx.sh
@@ -30,7 +30,7 @@ for target in "${CROSS_TARGETS[@]}"; do
     elif [[ $target == *"mingw"* ]]; then
         LIBCXX_CMAKE_FLAGS="$LIBCXX_CMAKE_FLAGS -DLIBCXX_HAS_WIN32_THREAD_API=ON"
     elif [[ $target == "wasm"* ]]; then
-        LIBCXX_CMAKE_FLAGS="$LIBCXX_CMAKE_FLAGS -DLIBCXX_HAS_MUSL_LIBC=ON"
+        LIBCXX_CMAKE_FLAGS="$LIBCXX_CMAKE_FLAGS -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_HAS_MUSL_LIBC=ON"
         if [[ $target == *"wamr"* ]]; then
             # Use exception handler from WAMR
             LIBCXXABI_CMAKE_FLAGS="$LIBCXXABI_CMAKE_FLAGS -DLIBCXXABI_ENABLE_EXCEPTIONS=OFF"
@@ -38,7 +38,6 @@ for target in "${CROSS_TARGETS[@]}"; do
         # Create empty fake libc++abi.a
         rm -f "$(target_install_prefix $target)/lib/libc++abi.a" || true
         "$OUTPUT_DIR/bin/llvm-ar" crs "$(target_install_prefix $target)/lib$LIBDIR_SUFFIX/libc++abi.a"
-    
     fi
 
     "$__CMAKE_WRAPPER" $target .. \
@@ -47,7 +46,7 @@ for target in "${CROSS_TARGETS[@]}"; do
         -DCMAKE_CXX_COMPILER_WORKS=1 \
         -DLLVM_LIBDIR_SUFFIX="$LIBDIR_SUFFIX" \
         -DLLVM_ENABLE_RUNTIMES="libcxxabi;libcxx" \
-        -DLIBCXX_ENABLE_SHARED=OFF \
+        -DLIBCXX_ENABLE_STATIC=ON \
         -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=TRUE \
         -DLIBCXX_CXX_ABI=libcxxabi \
         -DLIBCXX_INCLUDE_TESTS=OFF \
